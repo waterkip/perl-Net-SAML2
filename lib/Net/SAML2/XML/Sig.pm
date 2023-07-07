bda0f1ac2b6d10ec0389085293ac758b7313f71f
Wesley Schwengle <waterkip@cpan.org>
2023-07-07 14:22:44 -0400

    fixup! fixup! fixup! WIP: XML::Sig extending to place signature value elsewhere
    Signed-off-by: Wesley Schwengle <waterkip@cpan.org>

--- lib/Net/SAML2/XML/Sig.pm
+++ lib/Net/SAML2/XML/Sig.pm
@@ -11,6 +11,9 @@ use parent qw(XML::Sig);
 use XML::LibXML;
 use XML::LibXML::XPathContext;
 use URN::OASIS::SAML2 qw(:urn);
+use List::Util qw(uniq);
+
+my $DEBUG = 0;
 
 sub sign {
     my $self = shift;
@@ -25,10 +28,6 @@ sub _fix_signature_location {
 
     # saml-schema-protocol-2.0.xsd Schema hack
     #
-    # The real fix here is to fix XML::Sig to accept a XPATH to
-    # place the signature in the correct location.  Or use XML::LibXML
-    # here to do so
-    #
     # The protocol schema defines a sequence which requires the order
     # of the child elements in a Protocol based message:
     #
@@ -37,30 +36,72 @@ sub _fix_signature_location {
     # 1: saml:Issuer
     # 2: dsig:Signature
     #
-    # Seems like an oversight in the SAML schema specifiation but...
-    #
     my $dom = XML::LibXML->load_xml(string => $xml);
     my $xp  = XML::LibXML::XPathContext->new($dom);
     $xp->registerNs('saml', URN_ASSERTION);
     $xp->registerNs('samlp', URN_PROTOCOL);
     $xp->registerNs('ds', URN_SIGNATURE);
 
-    my $issuer = $xp->findnodes('//saml:Issuer');
-    return $xml unless $issuer->size;
-    $issuer = $issuer->get_node(1);
+    _remove_data_from_xml($xml, 'pre-fix.xml') if $DEBUG;
 
-    my $sig = $xp->findnodes('//ds:Signature');
-    return $xml unless $sig->size == 1;
+    my $issuers = $xp->findnodes('//saml:Issuer');
+    return $xml unless $issuers->size;
 
-    $sig = $sig->get_node(1);
-    $sig->unbindNode;
-    my $parent = $issuer->parentNode;
-    $parent->insertAfter($sig, $issuer);
+    $issuers->foreach(
+        sub {
+            my $issuer = $_;
+            my $parent = $issuer->parentNode;
+            my $ids = $xp->findnodes('//@ID', $issuer);
+            my @ids = uniq $ids->map(sub { $_->getValue });
+
+            my $sig = $xp->findnodes('//ds:Signature//@URI');
+            $sig->foreach(
+                sub {
+                    my $node = $_->parentNode->parentNode->parentNode;
+                    $node->unbindNode;
+                    $parent->insertAfter($node, $issuer);
+                }
+            );
+        }
+    );
 
     local $XML::LibXML::skipXMLDeclaration = $self->{no_xml_declaration};
-    return $xp->getContextNode->toString;
+    $xml = $xp->getContextNode->toString;
+
+    _remove_data_from_xml($xml, 'post-fix.xml') if $DEBUG;
+
+    return $xml;
 }
 
+sub _remove_data_from_xml {
+    my $xml = shift;
+    my $filename = shift;
+    return unless $DEBUG;
 
+    my $dom = XML::LibXML->load_xml(string => $xml);
+    my $xp  = XML::LibXML::XPathContext->new($dom);
+    $xp->registerNs('saml', URN_ASSERTION);
+    $xp->registerNs('samlp', URN_PROTOCOL);
+    $xp->registerNs('ds', URN_SIGNATURE);
+
+    $xp->findnodes('//dsig:SignatureValue')->foreach(
+        sub {
+            $_->removeChildNodes;
+            $_->appendText('Signature value removed');
+        }
+    );
+
+    $xp->findnodes('//dsig:X509Certificate')->foreach(
+        sub {
+            $_->removeChildNodes;
+            $_->appendText('X509Certificate value removed');
+        }
+    );
+
+    $xml = $xp->getContextNode->toString;
+    open my $fh, '>', $filename;
+    print $fh $xml;
+    close($fh);
+}
 
 1;
